9c4b86299b1d4dd850cd566ac0c2a8c1d9c05c06
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-02 20:11:33 -0300

    'Implementations to Tests access default Endpoint '

--- __tests__/test_login.py
+++ __tests__/test_login.py
@@ -1,13 +1,14 @@
 """Module Test Example functions."""
 import re
 from playwright.sync_api import Page, expect
+from config.environment import BASE_URL
 
 
 def test_has_title(page: Page):
     """Function Test Has Title."""
 
     # Arrage and Act
-    page.goto("http://alexfariakof.com:3000")
+    page.goto(BASE_URL)
 
     # Assert Expect a title "to contain" a substring.
     expect(page).to_have_title(re.compile("Despesas Pessoais"))
@@ -17,17 +18,17 @@ def test_get_started_link(page: Page):
     """Function Get Started Link."""
 
     # Arrange and Act
-    page.goto("http://alexfariakof.com:3000")
+    page.goto(BASE_URL)
 
     # Click the get started link.
     page.wait_for_selector("button:has-text('Entrar')")
     page.click("button:has-text('Entrar')")
 
     # Wait for the navigation to complete.
-    page.wait_for_url("http://alexfariakof.com:3000/dashboard", timeout=10000)
+    page.wait_for_url(f"{BASE_URL}/dashboard", timeout=10000)
 
     # Assert
-    assert page.url == "http://alexfariakof.com:3000/dashboard"
+    assert page.url == f"{BASE_URL}/dashboard"
     expect(page.locator("app-root")).to_be_visible()
     expect(page.locator("app-dashboard")).to_be_visible()
     expect(page.locator("app-layout")).to_be_visible()
