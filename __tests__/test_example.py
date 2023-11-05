8b1f16d9c8ab75eb2b1a6b57853a76fcf2119b5d
Ribeiro <42475620+alexfariakof@users.noreply.github.com>
2023-11-04 23:18:43 -0300

    Finish Test Implementations is Work Correctily

--- __tests__/test_example.py
+++ __tests__/test_example.py
@@ -11,4 +11,4 @@ def test_has_title(page: Page):
     page.goto(BASE_URL)
 
     # Assert Expect a title "to contain" a substring.
-    expect(page).to_have_title(re.compile("Despesa Pessoais"))
+    expect(page).to_have_title(re.compile("Despesas Pessoais"))
