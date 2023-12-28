b318187b589d7da0f07f11af7964eedd1da9217f
Ribeiro <alexfariakof@gmail.com>
2023-12-28 19:54:37 -0300

    Remoção do pylint e criação script de relatórios  de cobertura

---
+++ generate_coverage_report.react.ps1
@@ -0,0 +1,19 @@
+# Pasta onde o relatório será gerado
+$reportPath = ".\coverage"
+
+# Executa o testes e2e
+py -m pytest --env=local __tests__/.frontend_react --browser chromium --junitxml=./coverage/frontend_react_reporter.xml --html=./coverage/frontend_react_index.html
+
+# Encontra o diretório mais recente se for existente
+$latestDir = Get-ChildItem -Directory -Path $reportPath | Sort-Object LastWriteTime -Descending | Select-Object -First 1
+
+# Verifica se encontrou um diretório e, em caso afirmativo, obtém o nome do diretório (GUID)
+if ($latestDir -ne $null) {
+
+    # Abre a página index.html no navegador padrão do sistema operacional
+    Invoke-Item $reportPath\frontend_react_index.html
+}
+else {
+    Write-Host "Nenhum diretório de resultados encontrado."
+}
+
