91121295027caca86fde54545d84a9ca6389a6eb
Ribeiro <alexfariakof@gmail.com>
2024-04-16 18:05:27 -0300

    Update api.py

--- config/environments/dev/api.py
+++ config/environments/dev/api.py
@@ -3,4 +3,4 @@ from ..common import Config
 
 class ApiConfig(Config):
     """ Development Environment """
-    BASE_URL = "http://alexfariakof.com:42535"
+    BASE_URL = "http://alexfariakof.com:42536"
